d8a5140f2c93387182c0923789c7fb8edbbd98a0
Adam Kampff <adam.kampff@gmail.com>
2018-03-09 00:20:17 +0000

    Starting cFos analysis scripts

---
+++ Python_LIbraries/SZ_nii.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Library to handle NII stacks
+
+@author: Dreosti Lab
+"""
+# -----------------------------------------------------------------------------
+# Set "Library Path" - Social Zebrafish Repo
+lib_path = r'C:\Repos\Dreosti-Lab\Social_Zebrafish\Python_LIbraries'
+
+# Set Library Paths
+import sys
+sys.path.append(lib_path)
+
+# Import useful libraries
+import os
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image, ImageSequence
+
+# Utilities for processing NII stacks
+
+# Load NII stack
+def load_nii(path):
+ 
+    image = nib.load(path)
+    image_size = image.shape
+    image_type = image.get_data_dtype()
+    image_data = image.get_data() + 32768 # offset from 16-bit signed
+    image_data = np.transpose(image_data, (1, 0, 2)) # swap x/y
+    
+#    z_stack_avg = np.mean(image_data,axis=2)
+#    z_stack_max = np.max(image_data,axis=2)
+#    z_stack_std = np.std(image_data,axis=2)
+    
+#    plt.figure()
+#    plt.imshow(z_stack_max)
+    
+#    plt.figure()
+#    plt.imshow(z_stack_std)
+    
+ #   plt.figure()
+ #   plt.imshow(z_stack_avg)
+        
+    return image_data
+
+# Save NII stack
+def save_nii(path, data, affine, header):
+ 
+    new_img = nib.Nifti1Image(data, affine, header)
+    nib.save(new_img, path)
+
+# Load Mask Labels (TIFF)
+def load_mask(path):
+    
+    tiff = Image.open(path)
+    tiff.load()
+    num_frames = tiff.n_frames
+    images = np.zeros((tiff.height, tiff.width, num_frames), dtype=np.uint8) 
+    for i, page in enumerate(ImageSequence.Iterator(tiff)):
+        images[:,:,i] = np.reshape(page, (tiff.height, tiff.width))
+
+    return np.array(images)
+
+# FIN
---
+++ cFos/cFos_Process_Folder_List.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+This script loads and processes an NII folder list: .nii images and behaviour
+
+@author: Dreosti Lab
+"""
+# -----------------------------------------------------------------------------
+# Set "Library Path" - Social Zebrafish Repo
+lib_path = r'C:\Repos\Dreosti-Lab\Social_Zebrafish\Python_LIbraries'
+# -----------------------------------------------------------------------------
+# Set "Base Path" for this analysis session
+base_path = r'C:\Users\adamk\Desktop\cFos Experiments'
+
+# Set Library Paths
+import sys
+sys.path.append(lib_path)
+
+# Import useful libraries
+import glob
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+import SZ_nii as SZNII
+
+# Set Folder List
+folderListFile = base_path + r'\test_list.txt'
+
+# Read Folder List
+folderFile = open(folderListFile, "r")  #"r" means read the file
+folderList = folderFile.readlines()     # returns a list containing the lines
+num_folders = len(folderList) 
+folder_names = [] # We use this becasue we do not know the exact lenght
+nii_names = []
+npz_NS_names = []
+npz_S_names = []
+for i, f in enumerate(folderList):  #enumerate tells you what folder is 'i'
+    folder_name = base_path+f[:-1]
+    folder_names.append(folder_name)
+    
+    # Find cFos nii paths: "warped_red"
+    nii_name = glob.glob(folder_name + r'\*warped_red.nii.gz')[0]
+    nii_names.append(nii_name)
+    
+    # Find behaviour npz (NS and S)
+    npz_name_NS = glob.glob(folder_name + r'\Behaviour\*_NS.npz')[0]
+    npz_name_S = glob.glob(folder_name + r'\Behaviour\*_S.npz')[0]
+    npz_NS_names.append(npz_name_NS)
+    npz_S_names.append(npz_name_S)
+
+# Set Mask Path
+mask_path = base_path + r'\Masks\Caudal_Hypothalamus.labels.tif'
+
+# Load mask(s)
+mask_stack = SZNII.load_mask(mask_path)
+num_mask_voxels = np.sum(np.sum(np.sum(mask_stack)))
+# ------------------------------------------------------------------
+# Start Analysis
+
+# Analyze Behaviour (SPI for now)
+motion_values = np.zeros(num_folders)
+for i in range(num_folders):
+    data = np.load(npz_S_names[i])
+    tracking = data['tracking']
+    
+    fx = tracking[:,0] 
+    fy = tracking[:,1]
+    bx = tracking[:,2]
+    by = tracking[:,3]
+    ex = tracking[:,4]
+    ey = tracking[:,5]
+    area = tracking[:,6]
+    ort = tracking[:,7]
+    motion = tracking[:,8]
+    motion_values[i] = np.mean(motion)
+
+# Measure cFOS in Mask (normalize to "background"...eventually)
+cFos_values = np.zeros(num_folders)
+for i in range(num_folders):
+    data = SZNII.load_nii(nii_names[i])
+    cFos_values[i] = np.sum(np.sum(np.sum(mask_stack * data)))/num_mask_voxels
+    print(str(i) + ", cFos = " + str(cFos_values[i]) + ", motion = " + str(motion_values[i]))
+    
+    # Display
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(data[:,:,54])
+    plt.subplot(1,2,2)
+    plt.imshow(mask_stack[:,:,54] * data[:,:,54])
+
+# FIN

9f91bb145bc62fd9ffa9745e9da91c3b8138d934
elenadreo <e.dreosti@ucl.ac.uk>
2018-04-06 13:34:05 +0100

    Updated cFos for summary lists

--- cFos/Step1_cFos_Process_Folder_List.py
+++ cFos/Step1_cFos_Process_Folder_List.py
@@ -8,9 +8,6 @@ This script loads and processes a cFos folder list: .nii images and behaviour
 # Set "Library Path" - Social Zebrafish Repo
 lib_path = r'C:\Repos\Dreosti-Lab\Social_Zebrafish\libs'
 # -----------------------------------------------------------------------------
-# Set "Base Path" for this analysis session'
-base_path = r'C:\Users\adamk\Desktop\cFos Experiments'
-#base_path = r'D:\Registration'
 
 # Set Library Paths
 import sys
@@ -26,36 +23,21 @@ import SZ_analysis as SZA
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
-# Set Folder List
-#folderListFile = base_path + r'\Folder_list\wt_list.txt'
-folderListFile = base_path + r'\Folder_list\isolation.txt'
-#folderListFile = base_path + r'\Folder_list\No_SC.txt'
+# Set Summary List
+summaryListFile = r'\\128.40.155.187\data\D R E O S T I   L A B\Isolation_Experiments\Social_Brain_Areas_Analisys\Excel_Summary_List_ARK.xlsx'
 
 # Set Mask Path
-#mask_path = base_path + r'\Masks\Telencephalon_Area_1.labels.tif'
-#mask_path = base_path + r'\Masks\Telencephalon_Area_2.labels.tif'
-#mask_path = base_path + r'\Masks\Telencephalon_Area_8.tif'
-
-#mask_path = base_path + r'\Masks\Diencephalon_Area_1_Caudal_Hypothalamus.tif'
-#mask_path = base_path + r'\Masks\Diencephalon_Area_2_Habenula.labels.tif'
-#mask_path = base_path + r'\Masks\Diencephalon_Area_3.labels.tif'
-#mask_path = base_path + r'\Masks\Diencephalon_Area_4.tif'
-#mask_path = base_path + r'\Masks\Diencephalon_Area_7.labels.tif'
-
-mask_path = base_path + r'\Masks\Caudal_Hypothalamus.labels.tif'
+mask_path = r'D:\Anatomical_Segmentation\Diencephalon_Causal_Hypothalamus_Area1.tif'
 
 # Set Background Path
-#background_path = base_path + r'\Masks\Diencephalon_Area_10_DIL.tif'
-#background_path = base_path + r'\Masks\Diencephalon_Area_8.tif'
-
-background_path = base_path + r'\Masks\Tectum.labels.tif'
+background_path = r'D:\Anatomical_Segmentation\Diencephalon_Area_8.labels.tif'
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
-# Read folder list
-folder_names, test_ROIs, stim_ROIs, cfos_names, NS_names, S_names, fish_numbers = SZCFOS.read_folderlist(base_path, folderListFile)
-num_folders = len(folder_names)
+# Read summary list
+npz_files, cfos_files = SZCFOS.read_summarylist(summaryListFile)
+num_files = len(cfos_files)
 
 # Load mask(s)
 mask_stack = SZCFOS.load_mask(mask_path)
@@ -65,65 +47,91 @@ background_stack = SZCFOS.load_mask(background_path)
 num_background_voxels = np.sum(np.sum(np.sum(background_stack)))
 
 # ------------------------------------------------------------------
-# Start Analysis
+# Behaviour Analysis
+# ------------------------------------------------------------------
 
-# Analyze Behaviour for each folder (BPS and SPI for now)
-bps_values = np.zeros(num_folders)
-spi_values = np.zeros(num_folders)
-vpi_values = np.zeros(num_folders)
-dist_values = np.zeros(num_folders)
-for i in range(num_folders):
-    
-    # Set fish number 
-    fish_number = fish_numbers[i]
+# Freeze time threshold
+freeze_threshold = 500
 
-    # Load tracking data (S)
-    behaviour_data = np.load(S_names[i])
-    tracking = behaviour_data['tracking']
-    fx = tracking[:,0] 
-    fy = tracking[:,1]
-    bx = tracking[:,2]
-    by = tracking[:,3]
-    ex = tracking[:,4]
-    ey = tracking[:,5]
-    area = tracking[:,6]
-    ort = tracking[:,7]
-    motion = tracking[:,8]
-    
-    # Compute VPI (S)
-    VPI_s, AllVisibleFrames, AllNonVisibleFrames = SZA.computeVPI(bx, by, test_ROIs[i][fish_number-1], stim_ROIs[i][fish_number-1])
-    vpi_values[i] = VPI_s
+# Load Behaviour Analysis
+VPI_NS_ALL = np.zeros(num_files)
+VPI_S_ALL = np.zeros(num_files)
+SPI_NS_ALL = np.zeros(num_files)
+SPI_S_ALL = np.zeros(num_files)
+BPS_NS_ALL = np.zeros(num_files)
+BPS_S_ALL = np.zeros(num_files)
+Distance_NS_ALL = np.zeros(num_files)
+Distance_S_ALL = np.zeros(num_files)
+Freezes_NS_ALL = np.zeros(num_files)
+Freezes_S_ALL = np.zeros(num_files)
+OrtHist_NS_NSS_ALL = np.zeros((num_files,36))
+OrtHist_NS_SS_ALL = np.zeros((num_files,36))
+OrtHist_S_NSS_ALL = np.zeros((num_files,36))
+OrtHist_S_SS_ALL = np.zeros((num_files,36))
+Bouts_NS_ALL = np.zeros((0,10))
+Bouts_S_ALL = np.zeros((0,10))
+Pauses_NS_ALL = np.zeros((0,10))   
+Pauses_S_ALL = np.zeros((0,10))
 
-    # Compute SPI (S)
-    SPI_s, AllSocialFrames_TF, AllNONSocialFrames_TF = SZA.computeSPI(bx, by, test_ROIs[i][fish_number-1], stim_ROIs[i][fish_number-1])
-    spi_values[i] = SPI_s
-    
-    # Compute BPS (S)
-    BPS_s, avgBout_s = SZS.measure_BPS(motion)
-    bps_values[i] = BPS_s
-    
-    # Load tracking data (NS)
-    behaviour_data = np.load(NS_names[i])
-    tracking = behaviour_data['tracking']
-    fx = tracking[:,0] 
-    fy = tracking[:,1]
-    bx = tracking[:,2]
-    by = tracking[:,3]
-    ex = tracking[:,4]
-    ey = tracking[:,5]
-    area = tracking[:,6]
-    ort = tracking[:,7]
-    motion = tracking[:,8]
+# Go through all the npz files
+for f, filename in enumerate(npz_files):
 
-    # Compute Distance Traveled (NS)
-    dist_values[i] = SZA.distance_traveled(bx, by)
+    #Load each npz file
+    dataobject = np.load(filename)
+    
+    #Extract from the npz file
+    VPI_NS = dataobject['VPI_NS']    
+    VPI_S = dataobject['VPI_S']   
+    SPI_NS = dataobject['SPI_NS']    
+    SPI_S = dataobject['SPI_S']   
+    BPS_NS = dataobject['BPS_NS']   
+    BPS_S = dataobject['BPS_S']
+    Distance_NS = dataobject['Distance_NS']   
+    Distance_S = dataobject['Distance_S']   
+    OrtHist_ns_NonSocialSide = dataobject['OrtHist_NS_NonSocialSide']
+    OrtHist_ns_SocialSide = dataobject['OrtHist_NS_SocialSide']
+    OrtHist_s_NonSocialSide = dataobject['OrtHist_S_NonSocialSide']
+    OrtHist_s_SocialSide = dataobject['OrtHist_S_SocialSide']
+    Bouts_NS = dataobject['Bouts_NS']   
+    Bouts_S = dataobject['Bouts_S']
+    Pauses_NS = dataobject['Pauses_NS']   
+    Pauses_S = dataobject['Pauses_S']    
+
+    # Count Freezes
+    Freezes_NS_ALL[f] = np.sum(Pauses_NS[:,8] > freeze_threshold)
+    Freezes_S_ALL[f] = np.sum(Pauses_S[:,8] > freeze_threshold)
+    
+    #Make an array with all summary stats
+    VPI_NS_ALL[f] = VPI_NS
+    VPI_S_ALL[f] = VPI_S
+    SPI_NS_ALL[f] = SPI_NS
+    SPI_S_ALL[f] = SPI_S
+    BPS_NS_ALL[f] = BPS_NS
+    BPS_S_ALL[f] = BPS_S
+    Distance_NS_ALL[f] = Distance_NS
+    Distance_S_ALL[f] = Distance_S
+    OrtHist_NS_NSS_ALL[f,:] = OrtHist_ns_NonSocialSide
+    OrtHist_NS_SS_ALL[f,:] = OrtHist_ns_SocialSide
+    OrtHist_S_NSS_ALL[f,:] = OrtHist_s_NonSocialSide
+    OrtHist_S_SS_ALL[f,:] = OrtHist_s_SocialSide
+    
+    # Somehow concat all Pauses/Bouts
+    Bouts_NS_ALL = np.vstack([Bouts_NS_ALL, Bouts_NS])
+    Bouts_S_ALL = np.vstack([Bouts_S_ALL, Bouts_S])
+    Pauses_NS_ALL = np.vstack([Pauses_NS_ALL, Pauses_NS])
+    Pauses_S_ALL = np.vstack([Pauses_S_ALL, Pauses_S])
+
+
+# ------------------------------------------------------------------
+# Imaging Analysis
+# ------------------------------------------------------------------
 
 # Measure cFOS in Mask (normalize to "background")
-signal_values = np.zeros(num_folders)
-background_values = np.zeros(num_folders)
-normalized_cFos_values = np.zeros(num_folders)
-for i in range(num_folders):
-    cfos_data = SZCFOS.load_nii(cfos_names[i])
+signal_values = np.zeros(num_files)
+background_values = np.zeros(num_files)
+normalized_cFos_values = np.zeros(num_files)
+for i in range(num_files):
+    cfos_data = SZCFOS.load_nii(cfos_files[i])
     signal_value = np.sum(np.sum(np.sum(mask_stack * cfos_data)))/num_mask_voxels
     background_value = np.sum(np.sum(np.sum(background_stack * cfos_data)))/num_background_voxels
                              
@@ -131,7 +139,7 @@ for i in range(num_folders):
     signal_values[i] = signal_value
     background_values[i] = background_value
     normalized_cFos_values[i] = signal_value/background_value
-    print(str(i) + ", cFos = " + format(normalized_cFos_values[i], '.3f') + ", SPI = " + format(spi_values[i], '.3f') + ", VPI = " + format(vpi_values[i], '.3f'))
+    print(str(i) + ", cFos = " + format(normalized_cFos_values[i], '.3f') + ", VPI = " + format(VPI_S_ALL[i], '.3f') + ", BPS = " + format(BPS_S_ALL[i], '.3f'))
 
 # Make plots
 plt.figure()
@@ -139,47 +147,39 @@ plt.figure()
 # Plot unnormalized data
 plt.subplot(2,4,1)
 plt.title("BPS vs cFos - UnNormalized")
-plt.plot(bps_values, signal_values, 'b.')
-plt.plot(bps_values, background_values, 'r.')
+plt.plot(BPS_S_ALL, signal_values, 'b.')
+plt.plot(BPS_S_ALL, background_values, 'r.')
 
 plt.subplot(2,4,2)
-plt.title("SPI vs cFos - UnNormalized")
-plt.plot(spi_values, signal_values, 'b.')
-plt.plot(spi_values, background_values, 'r.')
+plt.title("VPI vs cFos - UnNormalized")
+plt.plot(VPI_S_ALL, signal_values, 'b.')
+plt.plot(VPI_S_ALL, background_values, 'r.')
 
 plt.subplot(2,4,3)
-plt.title("VPI vs cFos - UnNormalized")
-plt.plot(vpi_values, signal_values, 'b.')
-plt.plot(vpi_values, background_values, 'r.')
+plt.title("Freezes vs cFos - UnNormalized")
+plt.plot(Freezes_S_ALL, signal_values, 'b.')
+plt.plot(Freezes_S_ALL, background_values, 'r.')
 
 plt.subplot(2,4,4)
 plt.title("Distance (NS) vs cFos - UnNormalized")
-plt.plot(dist_values, signal_values, 'b.')
-plt.plot(dist_values, background_values, 'r.')
+plt.plot(Distance_NS_ALL, signal_values, 'b.')
+plt.plot(Distance_NS_ALL, background_values, 'r.')
 
 # Plot normalized data
 plt.subplot(2,4,5)
-plt.title("BPS vs cFos - Normalized by DA9")
-plt.plot(bps_values, normalized_cFos_values, 'k.')
+plt.title("BPS vs cFos - Normalized by DA8")
+plt.plot(BPS_S_ALL, normalized_cFos_values, 'k.')
 
 plt.subplot(2,4,6)
-plt.title("SPI vs cFos - Normalized by DA9")
-plt.plot(spi_values, normalized_cFos_values, 'k.')
+plt.title("VPI vs cFos - Normalized by DA8")
+plt.plot(VPI_S_ALL, normalized_cFos_values, 'k.')
 
 plt.subplot(2,4,7)
-plt.title("VPI vs cFos - Normalized by DA9")
-plt.plot(vpi_values, normalized_cFos_values, 'k.')
+plt.title("Freezes vs cFos - Normalized by DA8")
+plt.plot(Freezes_S_ALL, normalized_cFos_values, 'k.')
 
 plt.subplot(2,4,8)
-plt.title("Distance (NS) vs cFos - Normalized by DA9")
-plt.plot(dist_values, normalized_cFos_values, 'k.')
+plt.title("Distance (NS) vs cFos - Normalized by DA8")
+plt.plot(Distance_NS_ALL, normalized_cFos_values, 'k.')
 
 # FIN
-
-
-# Huh?
-#    x_min = min(test_ROIs[i][fish_number-1,0], stim_ROIs[i][fish_number-1,0])
-#    y_min = min(test_ROIs[i][fish_number-1,1], stim_ROIs[i][fish_number-1,1])
-#    x_max = max(test_ROIs[i][fish_number-1,0] + test_ROIs[i][fish_number-1,2], stim_ROIs[i][fish_number-1,0] + stim_ROIs[i][fish_number-1,2])
-#    y_max = max(test_ROIs[i][fish_number-1,1] + test_ROIs[i][fish_number-1,3], stim_ROIs[i][fish_number-1,1] + stim_ROIs[i][fish_number-1,3])
-

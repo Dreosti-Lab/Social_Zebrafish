59dbed3976ac686857f7193935551801e546f576
kampff <adam.kampff@gmail.com>
2019-09-11 14:21:11 +0100

    Added temporal bout prelim analysis

--- libs/SZ_summary.py
+++ libs/SZ_summary.py
@@ -83,7 +83,6 @@ def ort_histogram(ort):
 
     return ortHistogram
 
-
 # Analyze bouts and pauses (individual stats)
 def analyze_bouts_and_pauses(tracking, testROI, stimROI, visibleFrames, startThreshold, stopThreshold):
     
@@ -172,4 +171,74 @@ def analyze_bouts_and_pauses(tracking, testROI, stimROI, visibleFrames, startThr
     pauses[-1, 9] = visibleFrames[boutStops[-1]]
     return bouts, pauses
         
+# Analyze temporal bouts
+def analyze_temporal_bouts(bouts, binning):
+
+    # Determine total bout counts
+    num_bouts = bouts.shape[0]
+
+    # Determine largest frame number in all bouts recordings (make multiple of 100)
+    max_frame = np.int(np.max(bouts[:, 4]))
+    max_frame = max_frame + (binning - (max_frame % binning))
+    max_frame = 100 * 60 * 15 # 15 minutes
+
+    # Temporal bouts
+    visible_bout_hist = np.zeros(max_frame)
+    non_visible_bout_hist = np.zeros(max_frame)
+    frames_moving = 0
+    visible_frames_moving = 0
+    non_visible_frames_moving = 0
+    for i in range(0, num_bouts):
+        # Extract bout params
+        start = np.int(bouts[i][0])
+        stop = np.int(bouts[i][4])
+        duration = np.int(bouts[i][8])
+        visible = np.int(bouts[i][9])
+
+        # Ignore bouts beyond 15 minutes
+        if stop >= max_frame:
+            continue
+
+        # Accumulate bouts in histogram
+        if visible == 1:
+            visible_bout_hist[start:stop] = visible_bout_hist[start:stop] + 1
+            visible_frames_moving += duration
+        else:
+            non_visible_bout_hist[start:stop] = non_visible_bout_hist[start:stop] + 1
+            non_visible_frames_moving += duration
+        frames_moving += duration
+
+    #plt.figure()
+    #plt.plot(visible_bout_hist, 'b')
+    #plt.plot(non_visible_bout_hist, 'r')
+    #plt.show()
+
+    # Bin bout histograms
+    visible_bout_hist_binned = np.sum(np.reshape(visible_bout_hist.T, (binning, -1), order='F'), 0)
+    non_visible_bout_hist_binned = np.sum(np.reshape(non_visible_bout_hist.T, (binning, -1), order='F'), 0)
+
+    #plt.figure()
+    #plt.plot(visible_bout_hist_binned, 'b')
+    #plt.plot(non_visible_bout_hist_binned, 'r')
+    #plt.show()
+
+    # Compute Ratio
+    total_bout_hist_binned = visible_bout_hist_binned + non_visible_bout_hist_binned
+    vis_vs_non = (visible_bout_hist_binned - non_visible_bout_hist_binned) / total_bout_hist_binned
+
+    # Normalize bout histograms
+    #visible_bout_hist_binned = visible_bout_hist_binned / frames_moving
+    #non_visible_bout_hist_binned = non_visible_bout_hist_binned / frames_moving
+    #vis_v_non = visible_bout_hist_binned / non_visible_bout_hist_binned
+
+    # ----------------
+    # Temporal Bouts Summary Plot
+    #plt.figure()
+    #plt.plot(vis_vs_non, 'k')
+    #plt.ylabel('VPI')
+    #plt.xlabel('minutes')
+    #plt.show()
+
+    return vis_vs_non
+
 # FIN
